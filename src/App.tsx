a43bd72c3d23932579285be6399732a9f9b01f8e
Cemre <ckrts12@gmail.com>
2026-08-04 15:25:34 +0300

    DARK SIDE
    fix: remove unused useEffect hook and update CSS for supporting only the dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,8 @@ import { Routes, Route } from "react-router";
 import MainPage from "./pages/MainPage";
 import Layout from "./pages/Layout";
 import SearchResultsPage from "./pages/SearchResultsPage";
-import { useEffect } from "react";
 
 function App() {
-  useEffect(() => {
-    document.documentElement.classList.add("dark");
-  }, []);
-
   return (
     <Routes>
       <Route element={<Layout />}>
