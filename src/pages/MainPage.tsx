1378655026dd427a734194eaeb2c65d6302b9b5b
Cemre <ckrts12@gmail.com>
2026-08-04 15:23:13 +0300

    fix: remove unused useEffect hook from MainPage component

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,14 +1,9 @@
 import { MovieCategoryCarousel } from "@/components/MovieCategoryCarousel";
-import { useEffect } from "react";
 import { usePopularMovies } from "@/hooks/usePopularMovies";
 import { useTopRated } from "@/hooks/useTopRated";
 import { HeroCarousel } from "@/components/heroCarousel";
 
 function MainPage() {
-  useEffect(() => {
-    document.documentElement.classList.add("dark");
-  }, []);
-
   return (
     <div className='mt-24 p-2 md:mx-16 flex h-full flex-col gap-4 '>
       <HeroCarousel />
