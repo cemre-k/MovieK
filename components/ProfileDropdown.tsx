1439eb7aa7a3bbe18696ae310eebc8cd1c6fe3ac
Cemre <ckrts12@gmail.com>
2026-08-09 19:24:21 +0300

    refactor: remove unused Button import from ProfileDropdown component

--- components/ProfileDropdown.tsx
+++ components/ProfileDropdown.tsx
@@ -1,5 +1,4 @@
 import { useNavigate } from "react-router";
-import { Button } from "./ui/button";
 import { useAuth } from "@/hooks/useAuth";
 import { CurrentUserAvatar } from "./current-user-avatar";
 
